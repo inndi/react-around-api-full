93cbb38da109b3bf8056eb12608b94201935e447
Inna <inndi.spivakova@gmail.com>
2022-06-29 15:19:02 +0300

    fix: html res

--- backend/app.js
+++ backend/app.js
@@ -25,8 +25,24 @@ const {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-app.options('*', cors());
+// app.use(cors());
+// app.options('*', cors());
+
+app.use(function (req, res, next) {
+
+  res.header('Access-Control-Allow-Origin', "*");
+
+  const { method } = req;
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+
+    return res.end():
+  }
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/aroundb');
 
