7da16b7ee6d4a60eaa769dd22b63b2eb01d930c6
Inna <inndi.spivakova@gmail.com>
2022-06-29 15:22:15 +0300

    fix: html res

--- backend/app.js
+++ backend/app.js
@@ -36,6 +36,7 @@ app.use(function (req, res, next) {
   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
 
