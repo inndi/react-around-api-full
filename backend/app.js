31d753db655cc7819dc6c22860566aacb7370251
Inna <inndi.spivakova@gmail.com>
2022-07-06 18:14:38 +0300

    --adjust

--- backend/app.js
+++ backend/app.js
@@ -56,10 +56,6 @@ mongoose.connect('mongodb://localhost:27017/test', {
   useCreateIndex: true,
   useNewUrlParser: true,
   useUnifiedTopology: true
-}).then(() => {
-  console.log("connection is successfull");
-}).catch((e) => {
-  console.log("no connection ");
 });
 
 app.use(requestLogger);
