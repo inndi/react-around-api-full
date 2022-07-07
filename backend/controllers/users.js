0196de775c95bb972a5e184945ec1c8a2d5d22b0
Inna <inndi.spivakova@gmail.com>
2022-07-07 16:37:06 +0300

    --adjust

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -107,7 +107,7 @@ module.exports.login = (req, res, next) => {
       };
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        '4b0996963f9be042b22513a58bddaa061e7b9639840e0ad6687ebba5797cb992',
         { expiresIn: '7d' }
       );
       res.send({ token });
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -19,7 +19,7 @@ module.exports = (req, res, next) => {
   console.log('tok', token);
 
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');///////////////////key
+    payload = jwt.verify(token, '4b0996963f9be042b22513a58bddaa061e7b9639840e0ad6687ebba5797cb992');///////////////////key
   } catch (e) {
     const err = new Error('Authorization required');
     err.statusCode = 401;
